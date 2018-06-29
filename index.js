12e5104f858bff32fa934f74f19d58f382935a4c
Sam Ashtar <samashtarm@gmail.com>
2018-06-29 22:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key)
+var clone = Object.assign({}, object);
+delete clone[key];
+return clone;
